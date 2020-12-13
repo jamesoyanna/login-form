2ff11c61a1ff799dd8101dddac1db12b6a0efd77
sammybrave <52744168+sammybrave@users.noreply.github.com>
2020-12-13 23:29:28 +0100

    I just updated the login form

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector('#sam').addEventListener('click', function(){
+
+ alert('You have successfully registered')});
+
+
+document.querySelector('.submit').addEventListener('click', function(){
+
+ alert('Login successful')});
+
+ document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+               alert('Login successful');
+            }
+        });
+
+
+ document.querySelector('#sammy').addEventListener('click', function(){
+
+ alert('check your mail box for confirmation')});
